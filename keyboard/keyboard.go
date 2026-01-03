f3f4a4e69c6f19e96e19284ce05f373288a77e6f
AllMeatball <181806857+AllMeatball@users.noreply.github.com>
2026-01-02 18:45:36 -0600

    Add keyboard api

---
+++ keyboard/keyboard.go
@@ -0,0 +1,172 @@
+package keyboard
+
+import (
+	// "fmt"
+	"time"
+	"unicode"
+
+	"github.com/AllMeatball/u64-remote/server"
+)
+
+const (
+	KEYBOARD_BUFFER_OFS = 631
+	KEYBOARD_QUEUED_KEYS_OFS = 198
+	KEYBOARD_SHFLAG_OFS = 653
+
+	KEYBOARD_MAX_COUNT = 10
+)
+
+const (
+	KEY_DEL_INS = 20
+	KEY_RETURN  = 13
+)
+
+const (
+	SHFLAG_SHIFT     = 1
+	SHFLAG_COMMODORE = 2
+	SHFLAG_CONTROL   = 4
+)
+
+func StopTyping(server server.U64Server) {
+	server.PokeMemory(KEYBOARD_QUEUED_KEYS_OFS, []byte{0})
+}
+
+// Commodore 64 Programmer's Reference Guide (Appendix C: ASCII and CHR$ codes)
+var priv_unicode_remap = map[rune]byte{
+	' ': 32,
+	'!': 33,
+	'"': 34,
+	'#': 35,
+	'$': 36,
+	'%': 37,
+	'&': 38,
+	'\x27': 39, // single quote
+	'(': 40,
+	')': 41,
+	'*': 42,
+	'+': 43,
+	',': 44,
+	'-': 45,
+	'.': 46,
+	'/': 47,
+
+	'0': 48,
+	'1': 49,
+	'2': 50,
+	'3': 51,
+	'4': 52,
+	'5': 53,
+	'6': 54,
+	'7': 55,
+	'8': 56,
+	'9': 57,
+
+	':': 58,
+	';': 59,
+	'<': 60,
+	'=': 61,
+	'>': 62,
+	'?': 63,
+	'@': 64,
+
+	'[': 91,
+	'£': 92,
+	']': 93,
+
+	// Pi
+	'Π': 126,
+	'π': 126,
+}
+
+/*
+ * Sets shift flag bits.
+ * Usage: keyboard.SetShiftFlag(SHFLAG_SHIFT, true)
+ *
+ * NOTE: Currently this seems to not work (likely due to the key being raised after being set)
+ */
+func SetShiftFlag(server server.U64Server, mask byte, is_down bool) {
+	cur_shift_flag_bytes, err := server.PeekMemory(KEYBOARD_SHFLAG_OFS, 1)
+	_ = err
+
+	cur_shift_flag := cur_shift_flag_bytes[0]
+
+	if is_down {
+		 cur_shift_flag |= mask
+	}
+
+	err = server.PokeMemory(KEYBOARD_SHFLAG_OFS, []byte{cur_shift_flag})
+}
+
+func TypeBytes(server server.U64Server, keys []byte) {
+	slice_count  := len(keys) / 10
+	extra_keys := len(keys) % 10
+
+	keyboard_slices := [][]byte{}
+
+	slice_end := 0
+	for i := range slice_count {
+		slice_start := 10 * i
+		slice_end    = 10 * (i + 1)
+
+		keyboard_slice := keys[slice_start:slice_end]
+		keyboard_slices = append(keyboard_slices, keyboard_slice)
+	}
+
+
+	// Add rest of keys
+	keyboard_slices = append(keyboard_slices, keys[slice_end:slice_end + extra_keys])
+
+	// fmt.Println(keyboard_slices)
+
+	for _, slice := range keyboard_slices {
+		server.PokeMemory(KEYBOARD_BUFFER_OFS, slice)
+		server.PokeMemory(KEYBOARD_QUEUED_KEYS_OFS, []byte{byte(len(slice))})
+
+		key_queue_full := true
+		for key_queue_full {
+			time.Sleep(time.Millisecond * 50)
+			key_queue_data, err := server.PeekMemory(KEYBOARD_QUEUED_KEYS_OFS, 1)
+			if err != nil {
+				time.Sleep(time.Second * 2)
+				continue
+			}
+			keys_in_queue := key_queue_data[0]
+
+			key_queue_full = keys_in_queue > 1
+		}
+	}
+}
+
+func TypeChrCode(server server.U64Server, char byte) {
+	var c64_key []byte
+	c64_key = append(c64_key, char)
+
+	server.PokeMemory(KEYBOARD_BUFFER_OFS, c64_key)
+	server.PokeMemory(KEYBOARD_QUEUED_KEYS_OFS, []byte{1})
+}
+
+func UnicodeToPet(char rune) byte {
+	c64_key, has_char := priv_unicode_remap[char]
+
+	if has_char {
+		return c64_key
+	} else {
+		if unicode.IsLetter(char) {
+			return byte(char)
+		}
+	}
+
+	return priv_unicode_remap['?']
+}
+
+
+func TypeString(server server.U64Server, str string) {
+	var c64_keys []byte
+	for _, char := range str {
+		c64_key := UnicodeToPet(char)
+
+		c64_keys = append(c64_keys, c64_key)
+	}
+
+	TypeBytes(server, c64_keys)
+}
