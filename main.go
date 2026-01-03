e00fad8db9bff5ad85ccd7ab164c490e5f4ab589
AllMeatball <181806857+AllMeatball@users.noreply.github.com>
2026-01-03 13:09:16 -0600

    Add configurable config path options

--- main.go
+++ main.go
@@ -3,32 +3,74 @@ package main
 import (
 	"encoding/json"
 	"errors"
-	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
 	"github.com/AllMeatball/u64-remote/server"
 )
+
+
+func getConfigPaths() []string {
+	cfg_paths := []string{}
+
+	U64_CRED_PATH, has_cred_envar := os.LookupEnv("U64_CFG_PATH")
+
+	if has_cred_envar {
+		u64_cred_paths := strings.SplitSeq(U64_CRED_PATH, ":")
+
+		for path := range u64_cred_paths {
+			cfg_paths = append(cfg_paths, path)
+		}
+	}
+
+	cfg_paths = append(cfg_paths, "./")
+	cfg_dir, err := os.UserConfigDir()
+
+	if err != nil {
+		log.Printf("Can't get config path from (os.UserConfigDir): %v\n", err)
+	} else {
+		cfg_dir = filepath.Join(cfg_dir, "u64-remote")
+		cfg_paths = append(cfg_paths, cfg_dir)
+	}
+
+	return cfg_paths
+}
 
 func main() {
 	creds := server.U64Creds{
 		EnableMessageBox: true,
 	}
-	cfg_dir, err := os.UserConfigDir()
-	if err != nil {
-		fmt.Printf("Config dir not found using working directory: %s\n", cfg_dir)
-		cfg_dir = "./"
-	} else {
-		cfg_dir = filepath.Join(cfg_dir, "u64-remote")
+
+	cfg_paths := getConfigPaths()
+
+	config_loaded := false
+	for _, cfg_dir := range cfg_paths {
+		log.Printf("Trying config directory: %s\n", cfg_dir)
+		creds_path := filepath.Join(cfg_dir, "creds.json")
+
+		file, err := os.Open(creds_path)
+		if err != nil { continue }
+		defer file.Close()
+
+		err = json.NewDecoder(file).Decode(&creds)
+		if err != nil { continue }
+
+		config_loaded = true
+		log.Printf("Found final path: %s\n", cfg_dir)
+		break
 	}
 
-	creds_path := filepath.Join(cfg_dir, "creds.json")
+	if !config_loaded {
+		err_msg := "All config paths failed. valid directories are:"
 
-	file, err := os.Open(creds_path)
-	if err != nil { errHand_Fatal(err, creds.EnableMessageBox) }
-	defer file.Close()
+		for _, cfg_dir := range cfg_paths {
+			err_msg += "\n" + cfg_dir
+		}
 
-	err = json.NewDecoder(file).Decode(&creds)
-	if err != nil { errHand_Fatal(err, creds.EnableMessageBox) }
+		errHand_Fatal(errors.New(err_msg), creds.EnableMessageBox)
+	}
 
 	args := os.Args[1:]
 	if len(args) < 1 {
